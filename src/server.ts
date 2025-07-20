8c6a51debfe633ab35a674dc59924dca429d8193
Chomiii19 <jomariborines55@gmail.com>
2025-07-20 22:18:36 +0800

    Add websocket configurations and connections for messages

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,9 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import http from "http";
 import app from "./app";
+import { Server } from "socket.io";
+import socketServer from "./socket";
 
 dotenv.config();
 
@@ -9,13 +12,17 @@ const DB = `${process.env.DB}`.replace(
   "<db_password>",
   process.env.DB_PASSWORD as string,
 );
+const server = http.createServer(app);
+const io = new Server(server);
 
 mongoose
   .connect(DB)
   .then(() => {
     console.log("Successfully connected to DB...");
-    app.listen(PORT, () =>
-      console.log("Successfully connected to port: ", PORT),
-    );
+
+    server.listen(PORT, () => {
+      console.log("Successfully connected to port: ", PORT);
+      socketServer(io);
+    });
   })
   .catch((err) => console.error("Failed connection to DB:", err));
